f9d0cf3c20debf90cd6e5018cb28ec6385b80867
archshift <gh@archshift.com>
2015-12-25 15:59:23 -0800

    Install provided DejaVu font into static content directory

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ setup(
     packages=['adusk'],
     scripts=['bin/adusk'],
     data_files=[
-        ('share/adusk/cfg/', ["data/cfg/keyboard-layout.yaml"])
+        ('share/adusk/cfg/', ["data/cfg/keyboard-layout.yaml"]),
+        ('share/adusk/fonts/', ["data/fonts/DejaVuSansCondensed-Bold.ttf"])
     ],
     url='https://github.com/archshift/adusk',
     license='GPL3',
